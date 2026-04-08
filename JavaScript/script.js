7f5f26d3a381c029c4c5c7ec6d742fe5c3955213
Jash Vadgama <jashvadgama.ownai@gmail.com>
2026-04-08 17:59:14 +0530

    [WIP] Product Card

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,83 +1,93 @@
-// async function addData() {
-//   const response = await fetch(
-//     "https://ca3fa17a8f3d3f536b1a.free.beeceptor.com/api/products",
-//     {
-//         method: "POST",
-//         body: JSON.stringify({
-//             id: 1,
-//             name: "Smart_Phone",
-//             description: "OnePlus Nord CE 3 Lite 5G",
-//             price: 20000,
-//             category: "Electronics",
-//             stock: 200,
-//         }),
-//         headers: {
-//             "Content-type": "application/json; charset=UTF-8"
-//         }
-//     }
-//   );
-//   console.log(response);
-// }
-// addData()
+// const product = {
+//   id: 21,
+//   name: "SmartPhone",
+//   description: "OnePlus Nord CE 3 Lite 5G",
+//   price: 20000,
+//   category: "Electronics",
+//   stock: 200,
+// };
 
-// async function getData() {
-//   const response = await fetch(
-//     "https://ca3fa17a8f3d3f536b1a.free.beeceptor.com/api/products",
-//   ).then(response => {
-//       console.log(response);
-//   })
-// }
-// getData()
+// const form = document.getElementById("form")
 
-// fetch("https://fakestoreapi.com/products/2")
-//   .then((res) => res.json())
-//   .then((json) => console.log(json));
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault()
 
-// axios
-//   .delete("https://fakestoreapi.com/products/1")
-//   .then((response) => console.log("Deleted Data:", response.data));
+//   const id = document.getElementById("id")
+//   const name = document.getElementById("name")
+//   const desc = document.getElementById("desc")
+//   const price = document.getElementById("price")
+//   const category = document.getElementById("category")
+//   // console.log(id.value, name.value, desc.value, price.value, category.value);
 
-const product = {
-  id: 21,
-  name: "SmartPhone",
-  description: "OnePlus Nord CE 3 Lite 5G",
-  price: 20000,
-  category: "Electronics",
-  stock: 200,
-};
+//   const formObj = {
+//     id: id.value,
+//     name: name.value,
+//     desc: desc.value,
+//     price: price.value,
+//     category: category.value
+//   }
+// })
 
-// axios.post("https://fakestoreapi.com/products", product).then((response) => {
-//   console.log("Product Added:", response.data);
-// });
+const products = [
+  {
+    id: 1,
+    name: "OnePlus",
+    description: "OnePlus Nord CE 3 Lite 5G",
+    price: 20000,
+    category: "Electronics",
+    stock: 200,
+  },
+  {
+    id: 2,
+    name: "Iphone",
+    description: "Iphone 14 pro max with 128GB storage in matte black color.",
+    price: 56000,
+    category: "Electronics",
+    stock: 126,
+  },
+  {
+    id: 3,
+    name: "LeeCooper Shoes",
+    description: "Branded Shoes wrapped in real Leather.",
+    price: 3400,
+    category: "Footwear",
+    stock: 520,
+  },
+  {
+    id: 4,
+    name: "Dumbells",
+    description: "Dumbell Set each of 10kg. Rust free metal.",
+    price: 1500,
+    category: "Fitness",
+    stock: 100,
+  },
+];
 
-// axios
-//   .get("https://fakestoreapi.com/products")
-//   .then((response) => {
-//     const data = response.data
-//     console.log(data);
+const productGrid = document.getElementById("productGrid");
 
-//     data.forEach(p => {
-//       document.getElementById("dummyImg").setAttribute("src", p.image)
-//       console.log(p.image);
-//     })
-//   })
+productGrid.innerHTML = products
+  .map(
+    (product) =>
+      `
+       <div class="col-12 col-lg-3 col-md-6 d-flex justify-content-center align-items-center">
+          <div class="card w-100 border rounded-4 h-auto">
+            <div class="card-body">
+              <div class="h5 card-title mb-3">${product.name}</div>
 
-axios
-  .get("https://jsonplaceholder.typicode.com/users")
-  .then((res) => console.log(res.data));
-axios
-  .post("https://jsonplaceholder.typicode.com/users", product)
-  .then((res) => console.log(res.data));
-axios
-  .put("https://jsonplaceholder.typicode.com/users", {
-    data: {
-      id: 21,
-      name: "SmartPhone",
-      description: "OnePlus Nord CE 3 Lite 5G",
-      price: 20000,
-      category: "Electronics",
-      stock: 200,
-    },
-  })
-  .then((res) => console.log(res.data));
-// axios.delete("https://jsonplaceholder.typicode.com/users/1").then(res => console.log(res.data))
+              <p class="card-text">Description: ${product.description}</p>
+              
+              <p class="card-text">Category: ${product.category}</p>
+
+              <div class="d-flex justify-content-between">
+                <p class="card-text">Price: <span>$${product.price.toFixed(2)}</span> </p>
+                <p class="card-text">Stock: ${product.stock}</p>
+              </div>
+              <div class="d-flex justify-content-end align-items-center gap-2">
+                <button class="btn btn btn-primary border border-0 rounded-2">Edit</button>
+                <button class="btn btn btn-danger border border-0 rounded-2">Delete</button>
+              </div>
+            </div>
+          </div>
+       </div>`,
+  )
+  .join("");
